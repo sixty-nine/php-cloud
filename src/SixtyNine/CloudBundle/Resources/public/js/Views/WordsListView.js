45dd10605edd1847354ea1c8a73ed00dbea679d7
Daniel Barsotti <email>
2017-03-09 23:44:46 +0100

    Allow to remove words from lists - Resolves: #9

--- src/SixtyNine/CloudBundle/Resources/public/js/Views/WordsListView.js
+++ src/SixtyNine/CloudBundle/Resources/public/js/Views/WordsListView.js
@@ -5,17 +5,29 @@ SnCloud.Views.WordsList = Backbone.View.extend({
     el: 'div.container',
 
     events: {
-        'click table.words span.label': 'toggleOrientation',
-        'click a[data-type="palette"]': 'setPalette',
-        'click a[data-type="palette-type"]': 'changeColors'
+        'click span.orientation': 'toggleOrientation',
+//        'click a[data-type="palette"]': 'setPalette',
+//        'click a[data-type="palette-type"]': 'changeColors',
+        'click span.remove': 'removeWord'
     },
 
     initialize: function() {
         this.curPalette = $('a[data-type="palette"]:first').data('id');
     },
 
+    removeWord: function (e) {
+
+        // TODO: do it better :)
+        if (!confirm('Are you sure ?')) return;
+
+        var url = $(e.currentTarget).data('url'),
+            id = $(e.currentTarget).parent().data('id');
+
+        location.href = url.replace('9999', id);
+    },
+
     toggleOrientation: function (e) {
-        var wordId = $(e.currentTarget).data('id');
+        var wordId = $(e.currentTarget).parent().data('id');
         console.log('toggle', wordId);
     },
 
